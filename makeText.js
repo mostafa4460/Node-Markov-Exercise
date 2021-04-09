6aaa669c05e51e792af25646fa6704a8a1361e7d
Mostafa Nassr <mostafa.nassr88@gmail.com>
2021-04-09 01:04:56 -0400

    Step 2: Build the makeText.js Script

--- makeText.js
+++ makeText.js
@@ -1,2 +1,35 @@
 /** Command-line tool to generate Markov text. */
 
+const axios = require('axios'),
+    fs = require('fs'),
+    MarkovMachine = require('./markov');
+
+const path = process.argv[3];
+
+function makeTextFromFile() {
+    fs.readFile(path, "utf8", (err, data) => {
+        if (err) {
+            console.log(err);
+            process.exit(1);
+        }
+        const mm = new MarkovMachine(data);
+        console.log(mm.makeText());    
+    })
+}
+
+async function makeTextFromURL() {
+    try {
+        const {data} = await axios.get(path);
+        const mm = new MarkovMachine(data);
+        console.log(mm.makeText());    
+    } catch(err) {
+        console.log(err);
+        process.exit(1);
+    }
+}
+
+if (process.argv[2] === 'file') {
+    makeTextFromFile();
+} else if (process.argv[2] === 'url') {
+    makeTextFromURL();
+}
